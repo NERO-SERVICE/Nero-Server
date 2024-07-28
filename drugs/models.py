5ca38261cc9c15ed4afbe26be88d53dd4dad0964
pump9918 <hoyeon9918@gmail.com>
2024-07-29 00:58:38 +0900

    Feat : 모델 타입 선택 가능

--- drugs/models.py
+++ drugs/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 from accounts.models import User
 
+TYPE_CHOICES=(
+    ('아침','아침'),
+    ('점심','점심'),
+    ('저녁','저녁'),
+)
+
+DOSE_CHOICES=(
+    ('mg','mg'),
+    ('정','정'),
+)
+
 class DrugList(models.Model):
     id = models.AutoField(primary_key=True)
     drugName = models.CharField(max_length=100)
     capacity = models.PositiveIntegerField()
-    unit = models.CharField(max_length=20)
+    unit = models.CharField(max_length=10, choices=DOSE_CHOICES)
+    
+    def __str__(self):
+        return f"{self.drugName} {self.capacity}{self.unit}"
     
 class ClinicLog(models.Model):
     clinicId = models.AutoField(primary_key=True)
     drug = models.ForeignKey(DrugList, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.TextField()
-    clinicDate = models.DateTimeField()
-    nextDate = models.DateTimeField()
-    doseTime = models.CharField(max_length=100)
+    clinicDate = models.DateField()
+    nextDate = models.DateField()
+    doseTime = models.CharField(max_length=10, choices=TYPE_CHOICES)
     clinicNote = models.TextField()
--- records/models.py
+++ records/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-from today.models import Today
+
+SERVEY_TYPE_CHOICES=(
+    ('하루설문','하루설문'),
+    ('부작용','부작용'),
+    ('셀프기록','셀프기록'),
+)
+
+SYMPTOM_MAIN_TYPE_CHOICES=(
+    ('소화기계','소화기계'),
+    ('정신/심리','정신/심리'),
+    ('신경계','신경계'),
+    ('심혈관계','심혈관계'),
+    ('생식기계','생식기계'),
+    ('직접입력','직접입력'),
+)
 
 class QuestionList(models.Model):
     questionId = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=SERVEY_TYPE_CHOICES)
     content = models.TextField()
 
     def __str__(self):
@@ -12,14 +26,14 @@ class QuestionList(models.Model):
 class ServeyLog(models.Model):
     logId = models.AutoField(primary_key=True)
     questions = models.ForeignKey(QuestionList, on_delete=models.CASCADE)
-    today = models.ForeignKey(Today, on_delete=models.CASCADE)
+    today = models.DateTimeField(auto_now_add=True)
     answer = models.TextField()
     
 
 class SymptomCare(models.Model):
     symptomId = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=100)
-    content = models.TextField()
+    type = models.CharField(max_length=10, choices=SYMPTOM_MAIN_TYPE_CHOICES)
+    content = models.CharField(max_length=10)
 
     def __str__(self):
         return self.content
@@ -27,5 +41,5 @@ class SymptomCare(models.Model):
 class SymptomLog(models.Model):
     symptomLogId = models.AutoField(primary_key=True)
     symptomQuestions = models.ForeignKey(SymptomCare, on_delete=models.CASCADE)
-    today = models.ForeignKey(Today, on_delete=models.CASCADE)
+    today = models.DateTimeField(auto_now_add=True)
     answer = models.TextField()
