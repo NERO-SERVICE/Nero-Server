92c152241f6fd959d740d02153f24eca10c37c12
pump9918 <hoyeon9918@gmail.com>
2024-07-30 00:52:52 +0900

    Feat : 회원가입 username 삭제

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'nickname', 'is_superuser', 'is_active', 'is_staff', 'created_at', 'updated_at')
+        fields = ('id', 'email', 'is_superuser', 'is_active', 'is_staff', 'created_at', 'updated_at')
