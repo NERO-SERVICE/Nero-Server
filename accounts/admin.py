6100174fd2b9ff5a6b1ca17e7e25e6cc918f0dc6
pump9918 <hoyeon9918@gmail.com>
2024-07-30 01:07:15 +0900

    Chore : admin.py created_at 정보 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group
 from .models import User
 
 class UserAdmin(BaseUserAdmin):
+    readonly_fields = ('created_at', 'updated_at')
     list_display = ('email', 'is_staff', 'is_active', 'is_superuser')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
     fieldsets = (
@@ -18,7 +19,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}
         ),
     )
-    search_fields = ('email', 'nickname', 'sex')
+    search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
 
