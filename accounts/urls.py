ebb5e9cc755e3106f01ef27ffbcb5afeca9597a6
pump9918 <hoyeon9918@gmail.com>
2024-07-30 16:55:46 +0900

    Feat : 회원가입 성공시 201 상태 전송

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import UserCreateView
-
+# from .views import UserCreateView
+from .views import CustomRegisterView
 app_name = 'accounts'
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('signup/', UserCreateView.as_view(), name='signup'),
+    # path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/registration/', CustomRegisterView.as_view(), name='rest_register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,20 @@
-from rest_framework import generics
-from .serializers import UserSerializer
-from .models import User
+# from rest_framework import generics
+# from .serializers import UserSerializer
+# from .models import User
 
-class UserCreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserCreateView(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+from rest_framework import status
+from rest_framework.response import Response
+from dj_rest_auth.registration.views import RegisterView
+from .serializers import UserSerializer
+
+class CustomRegisterView(RegisterView):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
