3fb67feb81d1bfb66aaa4acff9880a645151d9ce
pump9918 <hoyeon9918@gmail.com>
2024-08-09 01:01:59 +0900

    Feat: uid String 지정

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from django.utils import timezone
 
 class User(AbstractUser):
-    uid = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     kakaoId = models.CharField(max_length=100, unique=True, null=True, blank=True)
     nickname = models.CharField(max_length=100, null=True, blank=True)
     createdAt = models.DateTimeField(default=timezone.now)
     updatedAt = models.DateTimeField(default=timezone.now)
-    temperature = models.IntegerField(default=36) 
+    temperature = models.FloatField(default=36.5) 
     email = models.EmailField(unique=True, null=True, blank=True, default='')
-    
+
     def __str__(self):
         return self.kakaoId
