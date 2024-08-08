5572d4d261d18b80d8c8601102dd767df05c875c
pump9918 <hoyeon9918@gmail.com>
2024-08-08 22:16:35 +0900

    Fix: 모델명 kakaoId 수정

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 class User(AbstractUser):
-    kakao_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    kakaoId = models.CharField(max_length=100, unique=True, null=True, blank=True)
     nickname = models.CharField(max_length=100, null=True, blank=True)
     createdAt = models.DateTimeField(default=timezone.now)
     updatedAt = models.DateTimeField(default=timezone.now)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import kakao_auth, get_user_info, check_nickname, signup_with_kakao
 
+app_name='accounts'
+
 urlpatterns = [
     path('auth/kakao/', kakao_auth, name='kakao_auth'),
     path('userinfo/<str:uid>/', get_user_info, name='get_user_info'),
