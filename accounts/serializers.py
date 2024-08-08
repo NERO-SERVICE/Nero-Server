4dbf0992a1c7bea8717ac9f0eb98f7c5637b9bb7
pump9918 <hoyeon9918@gmail.com>
2024-08-09 00:30:39 +0900

    Fix: kakaoId 변수명 통일

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,9 +4,9 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['uid', 'nickname', 'kakao_id', 'created_at', 'updated_at', 'temperature']
+        fields = ['uid', 'nickname', 'kakaoId', 'created_at', 'updated_at', 'temperature']
 
 class UserSignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['uid', 'kakao_id', 'nickname', 'created_at', 'updated_at', 'temperature']
+        fields = ['uid', 'kakaoId', 'nickname', 'created_at', 'updated_at', 'temperature']
