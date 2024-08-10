bbfaa1614c9defd16f0f1972a4011028a0fa1906
pump9918 <hoyeon9918@gmail.com>
2024-08-10 13:35:21 +0900

    Feat: views.py 유저 로그인 재정렬

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import kakao_auth, get_user_info, check_nickname
+from .views import kakao_auth, login
 from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name='accounts'
 
 urlpatterns = [
     path('auth/kakao/', kakao_auth, name='kakao_auth'),
-    path('userinfo/<str:uid>/', get_user_info, name='get_user_info'),
-    path('check-nickname/', check_nickname, name='check_nickname'),
+    path('login/', login, name='login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,15 @@
 import requests
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
-from .serializers import UserSerializer, UserSignUpSerializer
+from .serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-def get_tokens_for_user(uid):
-    user = get_object_or_404(User, id=uid)
-
+def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
     
     return {
@@ -19,10 +17,13 @@ def get_tokens_for_user(uid):
         'accessToken': str(refresh.access_token),
     }
 
+
+# 카카오 엑세스 토큰을 받으면 JWT accessToken과 refreshToken 생성 및 제공
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def kakao_auth(request):
     accessToken = request.data.get('accessToken')
+    kakaoId = request.data.get('kakaoId')
     nickname = request.data.get('nickname')
     createdAt = request.data.get('createdAt')
     updatedAt = request.data.get('updatedAt')
@@ -43,56 +44,34 @@ def kakao_auth(request):
             return JsonResponse({'error': 'Failed to retrieve user info from Kakao'}, status=400, json_dumps_params={'ensure_ascii': False})
 
         user, created = User.objects.get_or_create(kakaoId=kakaoId)
-
+        
         if created:
             user.nickname = nickname
             user.createdAt = createdAt
             user.updatedAt = updatedAt
             user.temperature = temperature
-            user.set_unusable_password()
+            user.set_unusable_password()  # 비밀번호를 사용하지 않는 계정
             user.save()
 
         # JWT 토큰 생성
-        tokens = get_tokens_for_user(user.id)
+        tokens = get_tokens_for_user(user)
         
-        serializer = UserSerializer(user)
-        return Response({'user': serializer.data, 'tokens': tokens}, status=status.HTTP_200_OK)
+        return Response({'tokens': tokens}, status=status.HTTP_200_OK)
 
     except requests.exceptions.RequestException as e:
         return JsonResponse({'error': 'Failed to retrieve user info from Kakao'}, status=500, json_dumps_params={'ensure_ascii': False})
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500, json_dumps_params={'ensure_ascii': False})
 
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def get_user_info(request, uid):
+
+# JWT accessToken을 보내면 유저 정보를 보냄
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication])
+def login(request):
     try:
-        user = User.objects.get(id=uid)
+        user = request.user
         serializer = UserSerializer(user)
-        return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False})
+        return Response({'user': serializer.data}, status=status.HTTP_200_OK)
     except User.DoesNotExist:
         return JsonResponse({'error': 'User not found'}, status=404, json_dumps_params={'ensure_ascii': False})
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def check_nickname(request):
-    nickname = request.GET.get('nickname')
-    if not nickname:
-        return JsonResponse({'error': 'Nickname is required'}, status=400, json_dumps_params={'ensure_ascii': False})
-
-    if User.objects.filter(nickname=nickname).exists():
-        return JsonResponse({'error': 'Nickname is already taken'}, status=400, json_dumps_params={'ensure_ascii': False})
-    else:
-        return JsonResponse({'message': 'Nickname is available'}, status=200, json_dumps_params={'ensure_ascii': False})
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def signup(request):
-    data = request.data
-    serializer = UserSignUpSerializer(data=data)
-    if serializer.is_valid():
-        user = serializer.save()
-        user.set_unusable_password()
-        user.save()
-        return JsonResponse(serializer.data, status=200, json_dumps_params={'ensure_ascii': False})
-    return JsonResponse(serializer.errors, status=400, json_dumps_params={'ensure_ascii': False})
