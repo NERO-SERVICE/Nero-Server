d6b81243b11a1e212e2a4dae137f673b168828fd
pump9918 <hoyeon9918@gmail.com>
2024-08-10 22:21:34 +0900

    Fit: 모델 구조 수정

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class DrfProduct(models.Model):
+    id = models.AutoField(primary_key=True)  # id 필드를 명시적으로 추가
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     productPrice = models.IntegerField(default=0)
