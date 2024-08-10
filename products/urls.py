c8ea7d906bd8ffd9770b34624992fdfe371857f7
pump9918 <hoyeon9918@gmail.com>
2024-08-11 00:04:35 +0900

    Fix: product id 통일

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ app_name = "products"
 urlpatterns = [
     path('', ListProductsView.as_view(), name='list_products'),
     path('create/', CreateProductView.as_view(), name='create_product'),
-    path('<int:docId>/', RetrieveProductView.as_view(), name='get_product'),
-    path('<int:docId>/update/', UpdateProductView.as_view(), name='update_product'),
-    path('<int:docId>/delete/', DeleteProductView.as_view(), name='delete_product'),
+    path('<int:id>/', RetrieveProductView.as_view(), name='get_product'),
+    path('<int:id>/update/', UpdateProductView.as_view(), name='update_product'),
+    path('<int:id>/delete/', DeleteProductView.as_view(), name='delete_product'),
 ]
