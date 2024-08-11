c549624b441fd2eaa65b356fb35e05af9c8d25cb
pump9918 <hoyeon9918@gmail.com>
2024-08-12 04:18:11 +0900

    Feat: 이미지 serializer 초기화

--- products/serializers.py
+++ products/serializers.py
@@ -1,41 +1,8 @@
-# from rest_framework import serializers
-# from .models import DrfProduct
-
-# class DrfProductSerializer(serializers.ModelSerializer):
-#     imageUrls = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = DrfProduct
-#         fields = [
-#             'id',
-#             'title',
-#             'description',
-#             'productPrice',
-#             'isFree',
-#             'imageUrls',
-#             'owner',
-#             'createdAt',
-#             'updatedAt',
-#             'viewCount',
-#             'status',
-#             'wantTradeLocation',
-#             'wantTradeLocationLabel',
-#             'categoryType',
-#             'likers',
-#         ]
-
-#     def get_imageUrls(self, obj):
-#         request = self.context.get('request')
-#         return [request.build_absolute_uri(image.file.url) for image in obj.imageFiles.all()]
-
 from rest_framework import serializers
-from .models import DrfProduct, ImageFile
+from .models import DrfProduct
 
 class DrfProductSerializer(serializers.ModelSerializer):
-    imageUrls = serializers.ListField(
-        child=serializers.URLField(), write_only=True, required=False
-    )
-    imageUrls = serializers.SerializerMethodField(read_only=True)  # 서버에서 응답 시 사용할 필드
+    imageUrls = serializers.SerializerMethodField()
 
     class Meta:
         model = DrfProduct
@@ -45,7 +12,7 @@ class DrfProductSerializer(serializers.ModelSerializer):
             'description',
             'productPrice',
             'isFree',
-            'imageUrls',  # 클라이언트와 서버 모두에서 'imageUrls' 사용
+            'imageUrls',
             'owner',
             'createdAt',
             'updatedAt',
@@ -60,12 +27,3 @@ class DrfProductSerializer(serializers.ModelSerializer):
     def get_imageUrls(self, obj):
         request = self.context.get('request')
         return [request.build_absolute_uri(image.file.url) for image in obj.imageFiles.all()]
-
-    def create(self, validated_data):
-        image_urls = validated_data.pop('imageUrls', [])
-        product = DrfProduct.objects.create(**validated_data)
-
-        for url in image_urls:
-            ImageFile.objects.create(product=product, file=url)
-
-        return product
