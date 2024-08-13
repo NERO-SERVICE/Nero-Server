46eb6e3b8005129362797b2a617c494726e151ff
pump9918 <hoyeon9918@gmail.com>
2024-08-13 16:21:39 +0900

    Feat: Create View 생성

--- clinics/urls.py
+++ clinics/urls.py
@@ -4,6 +4,8 @@ from .views import (
     RetrieveClinicView,
     UpdateClinicView,
     DeleteClinicView,
+    CreateClinicView,
+    CreateDrugView,
     ListDrugsView,
     RetrieveDrugView,
     UpdateDrugView,
@@ -14,10 +16,12 @@ app_name = "clinics"
 
 urlpatterns = [
     path('', ListClinicsView.as_view(), name='list_clinics'),
+    path('create/', CreateClinicView.as_view(), name='create_clinic'), 
     path('<int:clinicId>/', RetrieveClinicView.as_view(), name='retrieve_clinic'),
     path('<int:clinicId>/update/', UpdateClinicView.as_view(), name='update_clinic'),
     path('<int:clinicId>/delete/', DeleteClinicView.as_view(), name='delete_clinic'),
     path('<int:clinicId>/drugs/', ListDrugsView.as_view(), name='list_create_drugs'),
+    path('<int:clinicId>/drugs/create/', CreateDrugView.as_view(), name='create_drug'),
     path('<int:clinicId>/drugs/<int:drugId>/', RetrieveDrugView.as_view(), name='retrieve_drug'),
     path('<int:clinicId>/drugs/<int:drugId>/update/', UpdateDrugView.as_view(), name='update_drug'),
     path('<int:clinicId>/drugs/<int:drugId>/delete/', DeleteDrugView.as_view(), name='delete_drug'),
