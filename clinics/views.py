23b91d34b00f6a219ce8d3e5b670621b4f5f2269
pump9918 <hoyeon9918@gmail.com>
2024-08-13 16:33:43 +0900

    Feat: 클리닉 모델 중심 코드 재편

--- clinics/views.py
+++ clinics/views.py
@@ -1,33 +1,22 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound
-from .models import DrfClinics, DrfDrug
-from .serializers import DrfClinicsSerializer, DrfDrugSerializer
+from .models import DrfClinics
+from .serializers import DrfClinicsSerializer
 
-class ListClinicsView(generics.ListCreateAPIView):
+class ListClinicsView(generics.ListAPIView):
     serializer_class = DrfClinicsSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return DrfClinics.objects.filter(owner=self.request.user)
-        
+
 class CreateClinicView(generics.CreateAPIView):
     serializer_class = DrfClinicsSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-class RetrieveClinicView(generics.RetrieveAPIView):
-    serializer_class = DrfClinicsSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        clinic_id = self.kwargs.get('clinicId')
-        try:
-            return DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-        except DrfClinics.DoesNotExist:
-            raise NotFound("Clinic not found or you do not have permission to access it.")
 
 class UpdateClinicView(generics.UpdateAPIView):
     serializer_class = DrfClinicsSerializer
@@ -51,65 +40,7 @@ class DeleteClinicView(generics.DestroyAPIView):
         except DrfClinics.DoesNotExist:
             raise NotFound("Clinic not found or you do not have permission to access it.")
 
-class ListDrugsView(generics.ListCreateAPIView):
-    serializer_class = DrfDrugSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        clinic_id = self.kwargs.get('clinicId')
-        try:
-            clinic = DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-            return DrfDrug.objects.filter(item=clinic)
-        except DrfClinics.DoesNotExist:
-            raise NotFound("Clinic not found or you do not have permission to access it.")
-        
-class CreateDrugView(generics.CreateAPIView):
-    serializer_class = DrfDrugSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        clinic_id = self.kwargs.get('clinicId')
-        try:
-            clinic = DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-            serializer.save(item=clinic)
-        except DrfClinics.DoesNotExist:
-            raise NotFound("Clinic not found or you do not have permission to access it.")
-        
-class RetrieveDrugView(generics.RetrieveAPIView):
-    serializer_class = DrfDrugSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        clinic_id = self.kwargs.get('clinicId')
-        drug_id = self.kwargs.get('drugId')
-        try:
-            clinic = DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-            return DrfDrug.objects.get(drugId=drug_id, item=clinic)
-        except (DrfClinics.DoesNotExist, DrfDrug.DoesNotExist):
-            raise NotFound("Drug or Clinic not found or you do not have permission to access it.")
-
-class UpdateDrugView(generics.UpdateAPIView):
-    serializer_class = DrfDrugSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        clinic_id = self.kwargs.get('clinicId')
-        drug_id = self.kwargs.get('drugId')
-        try:
-            clinic = DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-            return DrfDrug.objects.get(drugId=drug_id, item=clinic)
-        except (DrfClinics.DoesNotExist, DrfDrug.DoesNotExist):
-            raise NotFound("Drug or Clinic not found or you do not have permission to access it.")
-
-class DeleteDrugView(generics.DestroyAPIView):
-    serializer_class = DrfDrugSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self):
-        clinic_id = self.kwargs.get('clinicId')
-        drug_id = self.kwargs.get('drugId')
-        try:
-            clinic = DrfClinics.objects.get(clinicId=clinic_id, owner=self.request.user)
-            return DrfDrug.objects.get(drugId=drug_id, item=clinic)
-        except (DrfClinics.DoesNotExist, DrfDrug.DoesNotExist):
-            raise NotFound("Drug or Clinic not found or you do not have permission to access it.")
+    def perform_destroy(self, instance):
+        # 이 클리닉에 속한 모든 drugs도 함께 삭제합니다.
+        instance.drugs.all().delete()
+        instance.delete()
