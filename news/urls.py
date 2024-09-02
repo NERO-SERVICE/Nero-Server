6aaf4baa51d5552b8dc58e39a8817e2295b60878
pump9918 <hoyeon9918@gmail.com>
2024-09-02 18:06:09 +0900

    Feat: 페이지네이션 적용 크롤링 뉴스 api

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import LatestNewsView, PaginatedNewsView
 
 app_name = "news"
 
 urlpatterns = [
+    path('latest/', LatestNewsView.as_view(), name='latest-news'),
+    path('', PaginatedNewsView.as_view(), name='news-list'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from .models import NewsArticle
+from .serializers import NewsArticleSerializer
 
-# Create your views here.
+class LatestNewsView(generics.ListAPIView):
+    queryset = NewsArticle.objects.order_by('-created_at')[:4]
+    serializer_class = NewsArticleSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class NewsPagination(PageNumberPagination):
+    page_size = 20
+
+class PaginatedNewsView(generics.ListAPIView):
+    queryset = NewsArticle.objects.all().order_by('-created_at')
+    serializer_class = NewsArticleSerializer
+    pagination_class = NewsPagination
