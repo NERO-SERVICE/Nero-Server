998f47a85479fc1074915e75d1536c8a0adfbac7
pump9918 <hoyeon9918@gmail.com>
2024-09-04 20:43:17 +0900

    Fix: 데이터 무결성 체크

--- clinics/serializers.py
+++ clinics/serializers.py
@@ -28,17 +28,28 @@ class DrfClinicsSerializer(serializers.ModelSerializer):
         drugs_data = validated_data.pop('drugs')
         clinic = DrfClinics.objects.create(**validated_data)
         for drug_data in drugs_data:
-            drug_archive = drug_data.pop('drugArchive')
-            DrfDrug.objects.create(item=clinic, drugArchive=drug_archive, **drug_data)
+            DrfDrug.objects.create(item=clinic, **drug_data)
         return clinic
+
     
     def update(self, instance, validated_data):
         drugs_data = validated_data.pop('drugs', None)
         instance = super().update(instance, validated_data)
+        
         if drugs_data:
-            instance.drugs.all().delete()
+            # 기존 drugs 업데이트 또는 추가
             for drug_data in drugs_data:
-                drug_archive = drug_data.pop('drugArchive')
-                DrfDrug.objects.create(item=instance, drugArchive=drug_archive, **drug_data)
+                drug_id = drug_data.get('drugId', None)
+                if drug_id:
+                    # 기존 drug 업데이트
+                    drug = DrfDrug.objects.get(drugId=drug_id, item=instance)
+                    drug.number = drug_data.get('number', drug.number)
+                    drug.initialNumber = drug_data.get('initialNumber', drug.initialNumber)
+                    drug.time = drug_data.get('time', drug.time)
+                    drug.allow = drug_data.get('allow', drug.allow)
+                    drug.save()
+                else:
+                    # 새로운 drug 추가
+                    DrfDrug.objects.create(item=instance, **drug_data)
+        
         return instance
-    
