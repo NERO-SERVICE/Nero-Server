435a64b75fe5c9d88320dc5750c50a93e518d2d3
pump9918 <hoyeon9918@gmail.com>
2024-09-15 00:48:56 +0900

    Fix: accounts url 정리

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('userinfo/', userinfo, name='userinfo'),
-    path('accounts/<int:userId>/update/', update_user_info, name='update_user_info'),
+    path('<int:userId>/update/', update_user_info, name='update_user_info'),
 ]
