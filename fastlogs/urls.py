9c7dd6ebb9c387ad27241c410e46d40a66a79c0f
pump9918 <hoyeon9918@gmail.com>
2024-09-17 16:13:56 +0900

    Feat: fastlogs URL 추가

--- fastlogs/urls.py
+++ fastlogs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SelfRecordListCreateView
+from .views import SelfRecordListCreateView, SelfRecordUpdateDeleteView, SelfRecordBulkUpdateView
 
 app_name = 'fastlogs'
 
 urlpatterns = [
     path('', SelfRecordListCreateView.as_view(), name='self-record-list-create'),
+    path('<int:pk>/', SelfRecordUpdateDeleteView.as_view(), name='self-record-update-delete'),
+    path('bulk/', SelfRecordBulkUpdateView.as_view(), name='self-record-bulk-update-delete'),
 ]
