de2fb42ddbef3ed46ccff1f1663675ba774029f3
pump9918 <hoyeon9918@gmail.com>
2024-09-23 15:35:08 +0900

    Fix: 생리주기 연간 데이터로 변경

--- menstruation/views.py
+++ menstruation/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from .serializers import MenstruationSerializer
 from rest_framework.permissions import IsAuthenticated
 from datetime import date
-import calendar
+
 
 class MenstruationListCreateView(generics.ListCreateAPIView):
     serializer_class = MenstruationSerializer
@@ -23,23 +23,30 @@ class MenstruationRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIVie
 
     def get_queryset(self):
         return Menstruation.objects.filter(owner=self.request.user)
-
+    
 class MenstruationCycleView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        year = int(request.GET.get('year'))
-        month = int(request.GET.get('month'))
+        year = request.GET.get('year')
 
-        # 해당 월의 첫 날과 마지막 날 계산
-        first_day_of_month = date(year, month, 1)
-        last_day_of_month = date(year, month, calendar.monthrange(year, month)[1])
+        if not year:
+            return Response({"error": "Year parameter is required."}, status=400)
 
-        # 필터링 로직 수정
+        try:
+            year = int(year)
+        except ValueError:
+            return Response({"error": "Year parameter must be an integer."}, status=400)
+
+        # 해당 연도의 첫 날과 마지막 날 계산
+        first_day_of_year = date(year, 1, 1)
+        last_day_of_year = date(year, 12, 31)
+
+        # 생리 주기 데이터를 해당 연도 전체로 필터링
         cycles = Menstruation.objects.filter(
             owner=request.user,
-            startDate__lte=last_day_of_month,
-            endDate__gte=first_day_of_month,
+            startDate__lte=last_day_of_year,
+            endDate__gte=first_day_of_year,
         )
 
         serializer = MenstruationSerializer(cycles, many=True)
