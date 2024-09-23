af7e8982dd1ce43841c35cc1c58af1003fde0c68
pump9918 <hoyeon9918@gmail.com>
2024-09-23 14:36:31 +0900

    Feat: 생리주기 연,월 관리

--- menstruation/urls.py
+++ menstruation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MenstruationListCreateView, MenstruationRetrieveUpdateDestroyView
+from .views import MenstruationListCreateView, MenstruationRetrieveUpdateDestroyView, MenstruationCycleView
 
 app_name = 'menstruation'
 
 urlpatterns = [
     path('', MenstruationListCreateView.as_view(), name='menstruation-list-create'),
     path('<int:pk>/', MenstruationRetrieveUpdateDestroyView.as_view(), name='menstruation-detail'),
+     path('/cycles/', MenstruationCycleView.as_view(), name='menstruation-cycles'),
 ]
