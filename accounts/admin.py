46ad73315314e2392944f1d19dfacc8b1ca1ded6
pump9918 <hoyeon9918@gmail.com>
2024-09-30 16:00:03 +0900

    Feat: Admin 계정 ui 변경

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,39 @@
 from django.contrib import admin
 from .models import User, Memories
+from django.utils import timezone
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'kakaoId', 'nickname', 'email', 'createdAt', 'birth', 'sex']
+    list_display = ['id', 'kakaoId', 'nickname', 'email', 'createdAt', 'birth', 'sex', 'is_product_writer', 'deleted_at']
     search_fields = ['nickname', 'email']
+    readonly_fields = ['id', 'createdAt', 'updatedAt']
+    list_filter = ['deleted_at', 'is_product_writer']
+    actions = ['soft_delete_users', 'restore_users']
+    
+    def get_queryset(self, request):
+        return User.all_objects.all()
+    
+    def soft_delete_users(self, request, queryset):
+        queryset.update(deleted_at=timezone.now())
+        self.message_user(request, "Selected users have been soft deleted.")
+    soft_delete_users.short_description = "Soft delete selected users"
+    
+    def restore_users(self, request, queryset):
+        queryset.update(deleted_at=None)
+        self.message_user(request, "Selected users have been restored.")
+    restore_users.short_description = "Restore selected users"
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.deleted_at:
+            return self.readonly_fields + ['nickname', 'email', 'sex', 'birth']
+        return self.readonly_fields
 
 @admin.register(Memories)
 class MemoriesAdmin(admin.ModelAdmin):
-    list_display = ['memoryId', 'get_user_nickname', 'items']
+    list_display = ['memoryId', 'get_user_nickname', 'items', 'deleted_at']
     search_fields = ['userId__nickname', 'items']
-
+    readonly_fields = ['memoryId', 'deleted_at']
+    
     def get_user_nickname(self, obj):
         return obj.userId.nickname
     
