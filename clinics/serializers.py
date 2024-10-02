e7e55bbc803d5afe70059381164ec8ddcb25ec16
pump9918 <hoyeon9918@gmail.com>
2024-10-02 16:57:10 +0900

    Feat: 클리닉 created 시간 자체 설정

--- clinics/serializers.py
+++ clinics/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Clinics, Drug, DrugArchive, MyDrugArchive
+from django.utils import timezone
 
 class MyDrugArchiveSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyDrugArchive
-        fields = ['myArchiveId', 'archiveId', 'drugName', 'target', 'capacity']
+        fields = ['myArchiveId', 'drug_archive', 'drugName', 'target', 'capacity']
         read_only_fields = ['myArchiveId']
 
 class DrugArchiveSerializer(serializers.ModelSerializer):
@@ -25,23 +26,27 @@ class DrugSerializer(serializers.ModelSerializer):
 class ClinicsSerializer(serializers.ModelSerializer):
     drugs = DrugSerializer(many=True)
     nickname = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Clinics
         fields = ['clinicId', 'owner', 'nickname', 'recentDay', 'nextDay', 'createdAt', 'updatedAt', 'description', 'drugs']
         read_only_fields = ['clinicId', 'owner', 'createdAt', 'updatedAt']
-    
+
     def get_nickname(self, obj):
         return obj.owner.nickname
-    
+
     def create(self, validated_data):
         drugs_data = validated_data.pop('drugs')
-        clinic = Clinics.objects.create(owner=self.context['request'].user, **validated_data)
+        
+        clinic = Clinics.objects.create(
+            owner=self.context['request'].user,
+            createdAt=timezone.now(),
+            **validated_data
+        )
     
         for drug_data in drugs_data:
             my_drug_archive_data = drug_data.pop('myDrugArchive')
-    
-            # MyDrugArchive 객체 생성
+
             my_drug_archive = MyDrugArchive.objects.create(
                 owner=clinic.owner,
                 archiveId=my_drug_archive_data['archiveId'],
@@ -49,8 +54,7 @@ class ClinicsSerializer(serializers.ModelSerializer):
                 target=my_drug_archive_data.get('target', ''),
                 capacity=my_drug_archive_data.get('capacity', '')
             )
-    
-            # Drug 객체 생성
+
             Drug.objects.create(
                 clinic=clinic,
                 myDrugArchive=my_drug_archive,
@@ -59,5 +63,5 @@ class ClinicsSerializer(serializers.ModelSerializer):
                 time=drug_data.get('time', '아침'),
                 allow=drug_data.get('allow', True)
             )
-    
+
         return clinic
