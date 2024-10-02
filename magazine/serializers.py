e55e6b0dfe40f072a568c1cb560cd9854dfd15c1
pump9918 <hoyeon9918@gmail.com>
2024-10-02 17:28:10 +0900

    Fix: 홈 serializer Imagefiles 추가:

--- magazine/serializers.py
+++ magazine/serializers.py
@@ -18,6 +18,7 @@ class MagazineSerializer(serializers.ModelSerializer):
             'title',
             'description',
             'imageUrls',
+            'imageFiles',
             'writer',
             'nickname',
             'createdAt',
