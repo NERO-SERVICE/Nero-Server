8cf069a4a8ccfc5f47ed97fbe35430fbc2900c4c
pump9918 <hoyeon9918@gmail.com>
2024-11-05 01:47:42 +0900

    Rollback: community 관련 세팅 주석처리

--- config/settings.py
+++ config/settings.py
@@ -112,8 +112,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
     ),
     'DEFAULT_CHARSET': 'utf-8',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
 }
 
 LOGIN_REDIRECT_URL = '/'
