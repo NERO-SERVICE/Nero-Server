19d57e9a997ff65bd575f66c0aec2866abec0ddb
pump9918 <hoyeon9918@gmail.com>
2024-11-07 14:03:38 +0900

    Feat: Rollback (로그인 유저정보 안보임 이슈)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from .models import User, Memories
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     userId = serializers.CharField(max_length=255, source='id')
@@ -26,14 +23,8 @@ class MemoriesSerializer(serializers.ModelSerializer):
         read_only_fields = ['memoryId', 'userId', 'deletedAt']
         
 class UserProfileUpdateInfoSerializer(serializers.ModelSerializer):
-    profile_image = serializers.SerializerMethodField()
+    profile_image = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
         fields = ['nickname', 'email', 'birth', 'sex', 'profile_image']
-
-    def get_profile_image(self, obj):
-        request = self.context.get('request')
-        if obj.profile_image:
-            return request.build_absolute_uri(obj.profile_image.url)
-        return None
